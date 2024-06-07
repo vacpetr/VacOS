cb360f1b94813cdf84238e5f21cff6091cf5518c
vacpetr <petr@vaclavek.net>
2024-06-07 17:58:48 +0200

    commit

---
+++ boot_sect.asm
@@ -0,0 +1,20 @@
+[org 0x7c00]
+
+bits 16	
+
+mov bx, TEST
+call print
+
+call print_nl
+
+jmp $
+
+%include "boot_sect_print.asm"
+
+TEST:
+    db 'test OSS', 0
+
+; Fill with 510 zeros minus the size of the previous code
+times 510-($-$$) db 0
+; Magic number
+dw 0xaa55 
