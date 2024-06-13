04e34ddce9d82a6249ed325d27563d58c1e38e21
vacpetr <petr@vaclavek.net>
2024-06-13 12:26:47 +0200

    neco

--- makefile
+++ makefile
@@ -2,6 +2,6 @@ compile:
 	nasm -f bin src/boot_sect.asm -o src/boot_sect.bin
 
 	dd if=/dev/zero of=boot.iso bs=512 count=100
-	dd if=src/boot_sect.bin of=boot.iso conv=notrunc bs=512 seek=0 count=2
+	dd if=src/boot_sect.bin of=boot.iso conv=notrunc bs=512 seek=0 count=1
 
 	qemu-system-x86_64 -drive file=boot.iso,format=raw,index=0
--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,26 +1,39 @@
 [org 0x7c00]
 
-bits 16	
-
 mov bp, 0x8000 ; set the stack safely away from us
 mov sp, bp
 
-mov bx, 0x1000 ; es:bx = 0x0000:0x9000 = 0x09000
-mov dh, 1
+mov bx, 0x1000 ; memory where disk segments are loaded
+mov dh, 1 ;number of segments to load
 
-call disk_load
+call disk_load ;load sectors from disk
 
-mov bx, TEST
-call print
+mov bx, TEST 
+call print ;print message
 
 call print_nl
 
-call print
-
-jmp 0x1000
+call switch_pm
 
 %include "src/boot_sect_print.asm"
 %include "src/boot_sect_disk.asm"
+%include "src/boot_sect_gdt.asm"
+%include "src/32bit_switch.asm"
+
+;-------------------------------------------------
+
+[bits 32]
+begin_pm:
+    VIDEO_MEMORY equ 0xb8000
+    WHITE_ON_BLACK equ 0x0f 
+
+    mov edx, VIDEO_MEMORY
+    mov ah, WHITE_ON_BLACK
+
+    jmp $
+
+
+;--------------------------------------------------
 
 TEST:
     db 'test OS', 0
@@ -30,10 +43,3 @@ TEST:
 times 510-($-$$) db 0
 ; Magic number
 dw 0xaa55 
-
-mov al, 'A'
-mov ah, 0x0e
-int 0x10
-
-mov eax, 0x1000
-jmp eax
